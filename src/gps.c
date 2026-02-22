7ad416cb3c536bd14facb46b668bbce546d748c6
Eugene Shelkovin <eugene.shelkovin@gmail.com>
2026-02-22 20:57:39 +0200

    Rework of the fix for https://github.com/fredzo/gpsdo-fw/issues/7

--- src/gps.c
+++ src/gps.c
@@ -463,9 +463,9 @@ void gps_parse(char* line)
                     month = 1;
                     year += 1;
                 }
-                else if( (day > 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10)) || // Other month with 31 days
-                         (day > 30 && (month == 4 || month == 6 || month == 9 || month == 11)) )                              // Other month with 30 days
-                {
+                else if( (day > 30 && (month == 4 || month == 6 || month == 9 || month == 11)) ||
+                         (day > 31))
+                {   // Case of other months with 30 or 31 days
                     day = 1;
                     month++;
                 }
